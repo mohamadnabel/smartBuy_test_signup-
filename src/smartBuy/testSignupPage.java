36600f35be35337a15388be81f453a59e72b245f
Mohammed Nabeel <128857044+mohamadnabel@users.noreply.github.com>
2024-03-16 04:48:41 +0300

    add navbar test

--- src/smartBuy/testSignupPage.java
+++ src/smartBuy/testSignupPage.java
@@ -20,22 +20,39 @@ public class testSignupPage {
 	public void mySetup() {
 		driver.get("https://smartbuy-me.com/smartbuystore/ar");
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(9));// even if the problem solve the implicitly
-		driver.manage().window().maximize();
+//		driver.manage().window().maximize();
 
 	};
 
 	@Test(invocationCount = 3, priority = 1)
 	public void myTest() throws InterruptedException {
+		Thread.sleep(4000);
+
+//		WebElement container = driver
+//				.findElement(By.cssSelector(".nav__links.nav__links--products.js-offcanvas-links"));
+//
+//		List<WebElement> listItems = container.findElements(By.tagName("li"));
+//
+//		int randomIndex = new Random().nextInt(listItems.size());
+//		Actions actions = new Actions(driver);
+//		actions.moveToElement(listItems.get(randomIndex)).perform();
+		WebElement navBarBtn = driver.findElement(By.linkText("منتجات ابل"));
+
+		WebElement ancerBtn = driver.findElement(By.linkText("منتجات ابل"));
+		ancerBtn.click();
+
 		Thread.sleep(2000);
 
-		WebElement container = driver.findElement(By.className("navigation__overflow"));
+		WebElement containera = driver.findElement(By.className("sub-navigation-list"));
 
-		List<WebElement> listItems = container.findElements(By.tagName("li"));
+		List<WebElement> listItemsa = containera.findElements(By.tagName("a"));
+		int randoma = new Random().nextInt(listItemsa.size());
+//		System.out.println(listItems.size());
+//		System.out.println(listItems.get(randomIndex).getText());
 
-		int randomIndex = new Random().nextInt(listItems.size());
+		listItemsa.get(randoma).click();
 
-		Actions actions = new Actions(driver);
-		actions.moveToElement(listItems.get(randomIndex)).perform();
+		driver.navigate().back();
 
 		// Now you can interact with any sub-menu or child elements that appear
 		// after the hover action
@@ -43,7 +60,7 @@ public class testSignupPage {
 		// Add your test logic here
 
 		// Close the browser (if needed)
-		// driver.quit();
+//		driver.quit();
 
 //		WebElement contenierTow = driver.findElement(By.className("sub-navigation-section"));
 //
